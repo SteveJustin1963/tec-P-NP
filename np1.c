8198f030953fcde36aba12ebf06db77f3e2d9634
Stephen Justin <stevejustin1963@gmail.com>
2023-09-14 14:08:05 +1000

    Update np1.c

--- np1.c
+++ np1.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h> // For dynamic memory allocation
 
 #define MAX_N 100
 #define MAX_SUM 10000
 
 int n;
-int set[MAX_N];
+int *set; // Dynamically allocated array
 int target;
-int dp[MAX_N+1][MAX_SUM+1];
+int **dp; // 2D Dynamically allocated array
 
+// Initialize the dp array
+void initializeDP() {
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= MAX_SUM; j++) {
+            dp[i][j] = -1;
+        }
+    }
+}
+
+// Check if there exists a subset with the given sum
 int subsetSum(int i, int sum) {
     if (i == n) {
         return sum == target;
@@ -20,22 +31,62 @@ int subsetSum(int i, int sum) {
 }
 
 int main() {
+    // Input for 'n'
     scanf("%d", &n);
+    if (n > MAX_N) {
+        printf("The number of elements exceeds the maximum limit.\n");
+        return 1;
+    }
+
+    // Dynamically allocate memory for 'set'
+    set = malloc(n * sizeof(int));
     for (int i = 0; i < n; i++) {
         scanf("%d", &set[i]);
     }
+
+    // Input for 'target'
     scanf("%d", &target);
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= MAX_SUM; j++) {
-            dp[i][j] = -1;
-        }
+    if (target > MAX_SUM) {
+        printf("The target sum exceeds the maximum limit.\n");
+        return 1;
     }
+
+    // Dynamically allocate memory for 'dp'
+    dp = malloc((n + 1) * sizeof(int *));
+    for (int i = 0; i <= n; i++) {
+        dp[i] = malloc((MAX_SUM + 1) * sizeof(int));
+    }
+
+    // Initialize dp array
+    initializeDP();
+
+    // Call the subsetSum function and print the result
     if (subsetSum(0, 0)) {
         printf("There exists a subset that sums to the target.\n");
     } else {
         printf("There does not exist a subset that sums to the target.\n");
     }
+
+    // Free dynamically allocated memory
+    free(set);
+    for (int i = 0; i <= n; i++) {
+        free(dp[i]);
+    }
+    free(dp);
+
     return 0;
 }
-;make sure that the input does not exceed the predefined MAX_N and MAX_SUM, 
-;otherwise you may need to dynamically allocate the dp table and the set.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+To improve the given code, we can make the following changes:
+
+1. Input Validation: Check that `n` and `sum` do not exceed `MAX_N` and `MAX_SUM`.
+2. Dynamic Memory Allocation: Optionally allocate `dp` and `set` dynamically based on the input size.
+3. Code Comments: Add comments to explain what each function does.
+4. Modularize the Initialization: Move the initialization of the `dp` array to a separate function.
+
+added checks for `n` and `target` 
+to make sure they don't exceed the predefined `MAX_N` and `MAX_SUM`. 
+used dynamic memory allocation for the `set` and `dp` arrays, which allows you to easily change `MAX_N` and `MAX_SUM` if necessary. 
+
+modularized the initialization of the `dp` array into a separate function called `initializeDP()`.
+
